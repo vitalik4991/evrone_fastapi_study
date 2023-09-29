740a94aa87f067f3d155e5e09509d40c73903c5a
Vitalik Tretiak <travianrulit11@gmail.com>
2023-09-29 18:21:13 +0300

    first comit

---
+++ evrone_fastapi/settings.py
@@ -0,0 +1,15 @@
+"""
+My settings
+"""
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    '''Settings class'''
+    main_url: str = '/'
+
+
+
+
+settings = Settings()
